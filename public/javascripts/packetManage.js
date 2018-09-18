f798961c442ec6b18fe7da338bea522e32972bf9
bonobonoya <toldme000@gmail.com>
2018-09-18 16:24:24 +0900

    bug fix - scroll box fix in client script

--- public/javascripts/packetManage.js
+++ public/javascripts/packetManage.js
@@ -9,6 +9,7 @@ $(function () {
     $('#name').attr('value', data.name);
     $('#color').attr('value', data.color);
     $("#chatBody").append(`<div><strong style="color:${data.color}">${data.name}</strong> has joined</div>`);
+    $('#scrollBox')[0].scrollTop = $('#scrollBox')[0].scrollHeight;
   });
 
   // print message in chat body
@@ -19,6 +20,7 @@ $(function () {
 
   socket.on('logout', function (data) {
     $("#chatBody").append(`<div><strong style="color:${data.color}">${data.name}</strong> has left</div>`);
+    $('#scrollBox')[0].scrollTop = $('#scrollBox')[0].scrollHeight;
   });
   // submit event on form
   $('#send').submit(function (e) {
