e67da648f4f42a24bd13aec2cd45e48a0b270ca6
bonobonoya <toldme000@gmail.com>
2018-09-18 10:53:29 +0900

    generate uid, color shift to server from client

--- public/javascripts/packetManage.js
+++ public/javascripts/packetManage.js
@@ -1,52 +1,15 @@
-// function getToServer() {
-//   $.ajax({
-//     type: 'POST',
-//     url: '/chat/get',
-//     data: null,
-//     dataType: 'json',
-//     success: function(data) {
-//       console.log(data);
-//       for (i in data) {
-//         $('#chatBody').append(`<li style="color: ${data[i].color};">${data[i].sender} - ${data[i].desc}</li>`);
-//         $('#scrollBox')[0].scrollTop = $('#scrollBox')[0].scrollHeight;
-//       }
-//       getToServer();
-//     },
-//     error: function(ajaxObj, error) {
-//       console.log('error', error);
-//       getToServer();
-//     }
-//   });
-// }
-
-// function sendToServer() {
-//   $.ajax({
-//     type: 'POST',
-//     url: '/chat',
-//     data: {
-//       sender: $('#sender').val(),
-//       desc: $('#desc').val(),
-//       color: $('#color').val()
-//     },
-//     success: function (data) {
-//       $('#desc').val('');
-//     }
-//   });
-//   return false;
-// }
-
 $(function () {
   var socket = io();
-
   // login part
   socket.emit("login", {
-    sender: $('#sender').val(),
-    color: $('color').val()
   });
 
   // login event in chat body
   socket.on("login", function (data) {
-    $("#chatBody").append(`<div><strong>${data}</strong> has joined</div>`);
+    console.log(data);
+    $('#sender').attr('value', data.sender);
+    $('#color').attr('value', data.color);
+    $("#chatBody").append(`<div><strong style="color:${data.color}">${data.sender}</strong> has joined</div>`);
   });
 
   // print message in chat body
@@ -69,22 +32,3 @@ $(function () {
   });
 });
 
-function generateUID() {
-  // I generate the UID from two parts here 
-  // to ensure the random number provide enough bits.
-  var firstPart = (Math.random() * 46656) | 0;
-  var secondPart = (Math.random() * 46656) | 0;
-  firstPart = ("000" + firstPart.toString(36)).slice(-3);
-  secondPart = ("000" + secondPart.toString(36)).slice(-3);
-  return firstPart + secondPart;
-}
-
-// for writing
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
--- socket.js
+++ socket.js
@@ -1,12 +1,39 @@
 var fs = require('fs');
 var sanitizeHTML = require('sanitize-html');
 
+// for writing
+function generateUID() {
+  // I generate the UID from two parts here 
+  // to ensure the random number provide enough bits.
+  var firstPart = (Math.random() * 46656) | 0;
+  var secondPart = (Math.random() * 46656) | 0;
+  firstPart = ("000" + firstPart.toString(36)).slice(-3);
+  secondPart = ("000" + secondPart.toString(36)).slice(-3);
+  return firstPart + secondPart;
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 module.exports = function (server) {
   var io = require('socket.io').listen(server);
-
+  var users = {};
   io.on('connection', function (socket) {
     socket.on('login', function (data) {
-      io.emit('login', data.sender);
+      var user = {
+        sender: generateUID(),
+        color: getRandomColor()
+      }
+      while (user.sender in users) {
+        user.sender = generateUID()
+      }
+      io.emit('login', user);
     });
 
     socket.on('send', function (data) {
